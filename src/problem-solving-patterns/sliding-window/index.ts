810fb85477b49bc1d4a4ce3b062aacc975047d2b
CamelJon <jonathan23986@gmail.com>
2025-10-07 17:56:38 +0300

    with performance testing

--- src/problem-solving-patterns/sliding-window/index.ts
+++ src/problem-solving-patterns/sliding-window/index.ts
@@ -1,29 +1,102 @@
+import { performance } from 'node:perf_hooks';
+
 /** Naive solution */
-function naive_sliding_window(input: any) {
-  // TODO: implement naive solution
-  return input;
+function naive_max_sub_array_sum(array: number[], n: number): number | null {
+  if (n > array.length) {
+    return null;
+  }
+
+  let max = -Infinity;
+
+  for (let i = 0; i < array.length; i++) {
+    let temp = 0;
+
+    for (let j = 0; j < n; j++) {
+      temp += array[i + j]!;
+    }
+
+    if (temp > max) {
+      max = temp;
+    }
+  }
+
+  return max;
 }
 
-/** Refactored solution */
-function sliding_window(input: any) {
-  // TODO: implement optimized solution
-  return input;
+/** Refactored solution - O(n) */
+function max_sub_array_sum(array: number[], n: number): number | null {
+  let max_sum = 0;
+  let temp_sum = 0;
+
+  if (array.length < n) {
+    return null;
+  }
+
+  for (let i = 0; i < n; i++) {
+    max_sum += array[i]!;
+  }
+
+  temp_sum = max_sum;
+
+  for (let i = n; i < array.length; i++) {
+    temp_sum = temp_sum - array[i - n]! + array[i]!;
+    max_sum = Math.max(max_sum, temp_sum);
+  }
+  return max_sum;
 }
 
 // Example test cases
-const test_case_1 = "test_case_1_input";
-const test_case_2 = "test_case_2_input";
-const test_case_3 = "test_case_3_input";
+const test_case_1 = [1, 2, 5, 2, 8, 1, 5];
+const test_case_2 = [4, 2, 1, 6];
+const test_case_3: number[] = [];
 
 // Naive tests
-const naive_test_1 = naive_sliding_window(test_case_1);
-const naive_test_2 = naive_sliding_window(test_case_2);
-const naive_test_3 = naive_sliding_window(test_case_3);
+let t0 = performance.now();
+const naive_test_1 = naive_max_sub_array_sum(test_case_1, 2);
+let t1 = performance.now();
+console.log(`Naive naive_test_1 took ${(t1 - t0).toFixed(4)} ms`);
+const naive_test_2 = naive_max_sub_array_sum(test_case_1, 4);
+t0 = performance.now();
+console.log(`Naive naive_test_2 took ${(t1 - t0).toFixed(4)} ms`);
+const naive_test_3 = naive_max_sub_array_sum(test_case_2, 1);
+t1 = performance.now();
+console.log(`Naive naive_test_3 took ${(t1 - t0).toFixed(4)} ms`);
+t0 = performance.now();
+const naive_test_4 = naive_max_sub_array_sum(test_case_2, 4);
+t1 = performance.now();
+console.log(`Naive naive_test_5 took ${(t1 - t0).toFixed(4)} ms`);
+const naive_test_5 = naive_max_sub_array_sum(test_case_3, 4);
+console.log(`Naive naive_test_5 took ${(t1 - t0).toFixed(4)} ms`);
 
 // Refactored tests
-const refactored_test_1 = sliding_window(test_case_1);
-const refactored_test_2 = sliding_window(test_case_2);
-const refactored_test_3 = sliding_window(test_case_3);
+t0 = performance.now();
+const refactored_test_1 = max_sub_array_sum(test_case_1, 2);
+t1 = performance.now();
+console.log(`Refactored refactored_test_1 took ${(t1 - t0).toFixed(4)} ms`);
+const refactored_test_2 = max_sub_array_sum(test_case_1, 4);
+t0 = performance.now();
+console.log(`Refactored refactored_test_2 took ${(t1 - t0).toFixed(4)} ms`);
+const refactored_test_3 = max_sub_array_sum(test_case_2, 1);
+t1 = performance.now();
+console.log(`Refactored refactored_test_3 took ${(t1 - t0).toFixed(4)} ms`);
+t0 = performance.now();
+const refactored_test_4 = max_sub_array_sum(test_case_2, 4);
+t1 = performance.now();
+console.log(`Refactored refactored_test_5 took ${(t1 - t0).toFixed(4)} ms`);
+const refactored_test_5 = max_sub_array_sum(test_case_3, 4);
+console.log(`Refactored refactored_test_5 took ${(t1 - t0).toFixed(4)} ms`);
 
-console.info("Naive results:", { naive_test_1, naive_test_2, naive_test_3 });
-console.info("Refactored results:", { refactored_test_1, refactored_test_2, refactored_test_3 });
+console.info('Naive results:', {
+  naive_test_1,
+  naive_test_2,
+  naive_test_3,
+  naive_test_4,
+  naive_test_5,
+});
+console.info('Refactored results:', {
+  refactored_test_1,
+  refactored_test_2,
+  refactored_test_3,
+  refactored_test_4,
+  refactored_test_5,
+});
